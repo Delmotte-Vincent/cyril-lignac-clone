4a97d94234d5552bfb97d7e9aed2b3d008989046
Delmotte-Vincent <delmotte-vincent@outlook.fr>
2023-04-16 13:41:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const navigationBar = document.querySelector(".nav-bar");
+const buttonReservation = document.querySelector(".reservation-button");
+
+const offset = navigationBar.offsetTop;
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > offset) {
+        navigationBar.classList.add('sticky');
+        buttonReservation.classList.add('button-style');
+   
+    } else {
+        navigationBar.classList.remove('sticky');
+        buttonReservation.classList.remove('button-style');
+    }
+  });
